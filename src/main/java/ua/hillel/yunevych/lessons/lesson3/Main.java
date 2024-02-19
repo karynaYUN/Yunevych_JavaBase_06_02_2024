73117a295d4f9662443e4686c706f38554c3c0d6
Karyna Yunevych <karyna.yun@educa.jcyl.es>
2024-02-19 16:14:34 +0100

    sm to try in github

--- src/main/java/ua/hillel/yunevych/lessons/lesson3/Main.java
+++ src/main/java/ua/hillel/yunevych/lessons/lesson3/Main.java
@@ -1,4 +1,17 @@
 package ua.hillel.yunevych.lessons.lesson3;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(10 % 1); //
+        System.out.println(10 % 2);
+        System.out.println(10 % 3);
+        System.out.println(10 % 4);
+        System.out.println(10 % 5);
+        System.out.println(10 % 6);
+        System.out.println(10 % 7);
+        System.out.println(10 % 8);
+        System.out.println(10 % 9);
+        System.out.println(10 % 10);
+        System.out.println(10 % 11);
+    }
 }
