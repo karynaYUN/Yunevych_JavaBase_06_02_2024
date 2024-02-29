4599b0b48122e9cfccc51c2d308933335f86d604
Karyna Yunevych <karyna.yun@educa.jcyl.es>
2024-02-29 22:13:55 +0100

    Home Work 7

---
+++ src/main/java/ua/hillel/yunevych/lessons/lesson6/HomeWork7.java
@@ -0,0 +1,37 @@
+package ua.hillel.yunevych.lessons.lesson6;
+
+import java.util.Scanner;
+
+public class HomeWork7 {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+
+            int User;
+            int run=0;
+            int MAX=10;
+            int MIN=0;
+            int MAX_run=3;
+            int Program =(int)(Math.random()*(MAX-MIN+1)+MIN);
+
+            System.out.println("ви маєте тільки "+MAX_run+" спроби");
+
+            while (run<MAX_run){
+                System.out.println("Напишіть число яке як ви думаєете загадала програма");
+                System.out.println("Це ваша "+(run+1)+" спроба");
+                User= in.nextInt();
+                in.nextLine(); //clean
+
+                if(User==Program){
+                    System.out.println("Ви вгадали число! Це число було "+Program);
+                } else if (Program<User) {
+                    System.out.println("Загадане число меньше");
+                }
+                else {
+                    System.out.println("Загадане число більше");
+                }
+                run++;
+            }
+            System.out.println("Нажаль ви не вгадали число!");
+        }
+    }
+
