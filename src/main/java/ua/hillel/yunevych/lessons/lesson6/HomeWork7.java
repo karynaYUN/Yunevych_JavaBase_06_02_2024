f9f780b62b23986a7dcd68a3af65e01b440f3fa3
Karyna Yunevych <karyna.yun@educa.jcyl.es>
2024-02-29 22:26:05 +0100

    Home Work 7

--- src/main/java/ua/hillel/yunevych/lessons/lesson6/HomeWork7.java
+++ src/main/java/ua/hillel/yunevych/lessons/lesson6/HomeWork7.java
@@ -6,32 +6,49 @@ public class HomeWork7 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int User;
-        int run=0;
-        int MAX=10;
-        int MIN=0;
-        int MAX_run=3;
-        int Program =(int)(Math.random()*(MAX-MIN+1)+MIN);
+                final int MIN = 0;
+                final int MAX = 10;
+                final int MAX_ATTEMPTS = 3;
 
-        System.out.println("ви маєте тільки "+MAX_run+" спроби");
+                int secretNumber = (int) (Math.random() * (MAX - MIN + 1)) + MIN;
+                int attempts = 0;
+                int guess;
 
-        while (run<MAX_run){
-            System.out.println("Напишіть число яке як ви думаєете загадала програма");
-            System.out.println("Це ваша "+(run+1)+" спроба");
-            User= in.nextInt();
-            in.nextLine(); //clean
+                System.out.println("Вгадайте число в діапазоні від " + MIN + " до " + MAX);
+                System.out.println("У вас є " + MAX_ATTEMPTS + " спроби.");
 
-            if(User==Program){
-                System.out.println("Ви вгадали число! Це число було "+Program);
-            } else if (Program<User) {
-                System.out.println("Загадане число меньше");
+                while (attempts < MAX_ATTEMPTS) {
+                    System.out.print("Введіть ваше число: ");
+
+                    while (!in.hasNextInt()) {
+                        System.out.println("Будь ласка, введіть ціле число.");
+                        in.next();
+                    }
+
+                    guess = in.nextInt();
+                    in.nextLine();
+
+                    if (guess < MIN || guess > MAX) {
+                        System.out.println("Введене число повинно бути в діапазоні від " + MIN + " до " + MAX + ".");
+                        continue;
+                    }
+
+                    if (guess == secretNumber) {
+                        System.out.println("Вітаємо! Ви вгадали число " + secretNumber);
+                        return;
+                    } else {
+                        System.out.println("Спроба " + (attempts + 1) + ": Не вірно.");
+                        if (guess < secretNumber) {
+                            System.out.println("Шукане число більше.");
+                        } else {
+                            System.out.println("Шукане число менше.");
+                        }
+                    }
+                    attempts++;
+                }
+
+                System.out.println("На жаль, ви не вгадали.Число було: " + secretNumber);
             }
-            else {
-                System.out.println("Загадане число більше");
-            }
-            run++;
         }
-        System.out.println("Нажаль ви не вгадали число!");
-    }
-}
+
 
