b93a21d24c2179ff1e185bf12ea8cd87bfbb544a
Karyna Yunevych <karyna.yun@educa.jcyl.es>
2024-02-29 22:15:49 +0100

    Home Work 7

--- src/main/java/ua/hillel/yunevych/lessons/lesson6/HomeWork7.java
+++ src/main/java/ua/hillel/yunevych/lessons/lesson6/HomeWork7.java
@@ -3,35 +3,35 @@ package ua.hillel.yunevych.lessons.lesson6;
 import java.util.Scanner;
 
 public class HomeWork7 {
-        public static void main(String[] args) {
-            Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
-            int User;
-            int run=0;
-            int MAX=10;
-            int MIN=0;
-            int MAX_run=3;
-            int Program =(int)(Math.random()*(MAX-MIN+1)+MIN);
+        int User;
+        int run=0;
+        int MAX=10;
+        int MIN=0;
+        int MAX_run=3;
+        int Program =(int)(Math.random()*(MAX-MIN+1)+MIN);
 
-            System.out.println("ви маєте тільки "+MAX_run+" спроби");
+        System.out.println("ви маєте тільки "+MAX_run+" спроби");
 
-            while (run<MAX_run){
-                System.out.println("Напишіть число яке як ви думаєете загадала програма");
-                System.out.println("Це ваша "+(run+1)+" спроба");
-                User= in.nextInt();
-                in.nextLine(); //clean
+        while (run<MAX_run){
+            System.out.println("Напишіть число яке як ви думаєете загадала програма");
+            System.out.println("Це ваша "+(run+1)+" спроба");
+            User= in.nextInt();
+            in.nextLine(); //clean
 
-                if(User==Program){
-                    System.out.println("Ви вгадали число! Це число було "+Program);
-                } else if (Program<User) {
-                    System.out.println("Загадане число меньше");
-                }
-                else {
-                    System.out.println("Загадане число більше");
-                }
-                run++;
+            if(User==Program){
+                System.out.println("Ви вгадали число! Це число було "+Program);
+            } else if (Program<User) {
+                System.out.println("Загадане число меньше");
             }
-            System.out.println("Нажаль ви не вгадали число!");
+            else {
+                System.out.println("Загадане число більше");
+            }
+            run++;
         }
+        System.out.println("Нажаль ви не вгадали число!");
     }
+}
 
